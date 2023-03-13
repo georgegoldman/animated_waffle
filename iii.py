dad488b5295711d76cd06ba97e4a5f9d41a20020
georgegoldman <georgegoldmanjohn.o@gmail.com>
2023-03-13 18:45:21 +0100

    done from number 1 to 3

--- iii.py
+++ iii.py
@@ -3,8 +3,7 @@ def list_of_orders_total_prices ():
     new_list_price = []
     get_list_price_String = input('''
 list of orders total prices of the current week in AUD
-format 100,33,744,...n
-    ''')
+format 100,33,744,...n: ''')
     list_price = get_list_price_String.split(",")
     for x in list_price:
         new_list_price.append(float(x))
@@ -13,19 +12,27 @@ format 100,33,744,...n
 
 class Sale:
 
-    total_price_curr_week = list_of_orders_total_prices ()
-    total_price_pre_week = list_of_orders_total_prices ()
+    total_sales_price_curr_week = list_of_orders_total_prices ()
+    total_sales_price_pre_week = list_of_orders_total_prices ()
+    tota_number_of_person_visited_curr = int(input("Total number of persons visited in the Current Week: "))
+    tota_number_of_person_visited_pre = int(input("Total number of persons visited in the Last Week: "))
 
     def __init__(self):
         return
     
-    def total_sale_for_current_week(self):
+    def get_total_sale(self,total_sales):
         a = 0
-        for y in self.total_price_curr_week:
+        for y in total_sales:
             a+= y
         return a
+    
+    def current_Week_per_person_average_sale(self):
+        print(f"Current Week per person average sale= {self.get_total_sale(self.total_sales_price_curr_week)/self.tota_number_of_person_visited_curr}")
+    
+    def previous_Week_per_person_average_sale(self):
+        print(f"Last Week per person average sale= {self.get_total_sale(self.total_sales_price_pre_week)/self.tota_number_of_person_visited_pre}")
 
 sale1 = Sale()
-
-print(sale1.total_price_curr_week)
-print(sale1.total_sale_for_current_week())
+sale1.current_Week_per_person_average_sale()
+sale1.previous_Week_per_person_average_sale()
+# print(sale1.get_total_sale(sale1.total_sales_price_pre_week))
