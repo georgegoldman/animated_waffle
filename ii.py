1217313a81a39e828d87b1065c5b5943e3418dfd
georgegoldman <georgegoldmanjohn.o@gmail.com>
2023-03-13 18:19:33 +0100

    working on sale iii

--- ii.py
+++ ii.py
@@ -1,16 +1,19 @@
+#create Restaurant
 class Restaurant:
     x = int(input("enter length meter(s): "))
     y = int(input("enter width meter(s): "))
 
     def __init__(self):
         return
-    
+
     def size(self):
         return self.x * self.y
 
     def capacity(self):
         return float(self.size()/1.2 )
     
+
+#instantiate restaurant object
 restaurant_a = Restaurant()
 
 if restaurant_a.capacity() > 60:
--- iii.py
+++ iii.py
@@ -1,21 +1,31 @@
-
+#get list of order total prices and return a list
 def list_of_orders_total_prices ():
-    get_list_price_String = int(input('''
-    list of orders total prices of the current week in AUD
-    format 100,33,744,...n
-    '''))
-    list_price = DOB.split(",")
-    if len(DOB_list) < 3:
-         print("Wrong Date of birth format...\nExiting signup...")
-         exit()
-    else:
-         return date(int(DOB_list[0]), int(DOB_list[1]), int(DOB_list[2]))
+    new_list_price = []
+    get_list_price_String = input('''
+list of orders total prices of the current week in AUD
+format 100,33,744,...n
+    ''')
+    list_price = get_list_price_String.split(",")
+    for x in list_price:
+        new_list_price.append(float(x))
+    return new_list_price
+    
 
 class Sale:
 
-    total_price_curr_week = cin_list()
-    total_price_pre_week = cin_list()
+    total_price_curr_week = list_of_orders_total_prices ()
+    total_price_pre_week = list_of_orders_total_prices ()
 
     def __init__(self):
         return
+    
+    def total_sale_for_current_week(self):
+        a = 0
+        for y in self.total_price_curr_week:
+            a+= y
+        return a
 
+sale1 = Sale()
+
+print(sale1.total_price_curr_week)
+print(sale1.total_sale_for_current_week())
