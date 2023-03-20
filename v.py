2169167b615209c3723069bbe64e6e75565c5b67
georgegoldman <georgegoldmanjohn.o@gmail.com>
2023-03-20 07:06:55 +0100

    refactored

--- v.py
+++ v.py
@@ -1,26 +1,11 @@
-def cin_int (message):
-    try:
-        d = int(input(message))
-        return d
-    except:
-        print("numbers only")
+address = input("full address: ")
+distance = int(input("Distance between in Kilometre: "))
 
-class Order:
-    address = input("full address: ")
-    distance = cin_int("Distance between in Kilometre: ")
-
-    def __init__(self) -> None:
-        pass
-
-    def delivery_charge(self):
-        if self.distance > 0 and self.distance <= 5:
-            print("$5")
-        elif self.distance > 5 and self.distance <= 10:
-            print("$8")
-        elif self.distance > 10 and self.distance <= 12:
-            print("$10")
-        else:
-            print("No Delivery can be done.")
-
-order_x = Order()
-order_x.delivery_charge()
+if distance > 0 and distance <= 5:
+    print("$5")
+elif distance > 5 and distance <= 10:
+    print("$8")
+elif distance > 10 and distance <= 12:
+    print("$10")
+else:
+    print("No Delivery can be done.")
--- vi.py
+++ vi.py
@@ -1,26 +1,10 @@
-print("Ctrl+z to exit app")
+enter_order = int(input("Insert the order base cost in AUD:\t"))
+enter_order_type = int(input("enter order type 1 for dine in, 2 for pick up, 3 for deivery:\t"))
 
-def cin_int(message):
-    try:
-        return int(input(message))
-    except:
-        print("please enter numbers only")
-
-class OrderCharges:
-    order_charges = {
+order_charges = {
         1: ["dine in", 8],
         2: ["pick up", 0],
         3: ["delivery", 10]
     }
-    enter_order = cin_int("Insert the order base cost in AUD:\t")
-    enter_order_type = cin_int("enter order type eg 1- dine in, 2-pick up, 3-deivery:\t")
-    def __init__(self) -> None:
-        pass
 
-    def calculate_charge(self):
-        print(self.enter_order + (self.enter_order * (self.order_charges[self.enter_order_type][1]*0.01)))
-
-
-
-orderCharges_x = OrderCharges()
-orderCharges_x.calculate_charge()
+print(enter_order + (enter_order * (order_charges[enter_order_type][1]*0.01)))
