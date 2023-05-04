331b29c4e8443661e013f22e0bbed742891ba950
georgegoldman <georgegoldmanjohn.o@gmail.com>
2023-05-04 20:42:38 +0100

    fixed the errors

--- MIS501_Assessment3/app.py
+++ MIS501_Assessment3/app.py
@@ -37,11 +37,69 @@ def ordering(option):
 
 
 def order_online():
-    input(" Enter 1 for Self Pickup.\n Enter 2 for Home Delivery.\n Enter 3 to go to previous Menu.")
+    order_option = """ Enter 1 for Self Pickup. 
+ Enter 2 for Home Delivery.
+ Enter 3 to go to previous Menu."""
+    order_option_value = input(order_option)
+    if (order_option_value == "1" or order_option_value == 2):
+        menu(order_option_value)
+    elif(order_option_value == 3):
+        ordering(None)
+    else:
+        print("Please a valid option")
+        order_online()
 
-def menu(order_online_option):
-    menu = """Enter 1 for Noodles Price AUD 2
-    Enter 2 for Sandwich Price AUD 4
-    Enter 3 for Dumpling Price  AUD 6
-    Enter 4 for Muffins Price AUD 8
-    Enter 5 for Pasta Price AUD 10"""
+def menu(order_online_value):
+    print(order_online_value)
+    menu_option = """ Enter 1 for Noodles Price AUD 2
+ Enter 2 for Sandwich Price AUD 4
+ Enter 3 for Dumpling Price  AUD 6
+ Enter 4 for Muffins Price AUD 8
+ Enter 5 for Pasta Price AUD 10
+ Enter 6 for Pizza Price AUD 20
+ Enter 7 for Drinks Menu:
+ """
+    menu_option_value = input(menu_option)
+    if (menu_option_value == "7"):
+        print("loading option 7")
+    elif (menu_option in ["1","2","3","4","5","6"]):
+        print("order options...")
+    else:
+        print("please select a valid option")
+
+def drink_menu(menu_option_value):
+    print(menu_option_value)
+    dink_option = """Enter 1 for Coffee Price AUD 2
+ Enter 2 for Colddrink Price AUD 4
+ Enter 3 for Shake Price AUD 6
+ Enter 4 fro Checkout:
+ """
+    drink_option_value = input(dink_option)
+    if (drink_option_value in ["1", "2", "3"]):
+        print("you selected a drink")
+    elif drink_option_value == "4":
+        check_out = """Please Enter Y to proceed to Checkout or
+ Enter N to cancel the order """
+        check_out_value = input(check_out)
+        if check_out_value.lower() == "y":
+            print("Proceeding to checkout")
+        elif check_out_value.lower() == "n":
+            print("cancling check out")
+        else:
+            print("please select a valid option")
+    
+def no_address_alert():
+    msg = """You have not mentioned your address, while signing up.
+ Please Enter Y if you would like to enter your address.
+ Enter N if you would like to select mode of order
+ """
+    msg_option = input(msg)
+    if (msg_option.lower() == "y"):
+        print("you selected y")
+    elif (msg_option.lower() == "n"):
+        print("you selected n")
+    else:
+        print("please enter a valid option")
+
+def calculator():
+    
