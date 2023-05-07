924760561fc44c8d3bc97327614a3c0ee32c01ea
georgegoldman <georgegoldmanjohn.o@gmail.com>
2023-05-07 03:39:43 +0100

    fixed

--- MIS501_Assessment3/app.py
+++ MIS501_Assessment3/app.py
@@ -2,67 +2,31 @@
     
 
 
-def calculator(mode):
-    if (mode == "dine in"):
-        print("dine in mode")
-    elif (mode == "click and collect"):
-        print("click and collect")
-    elif (mode == "delivery"):
-        print("delivery mode")
-    else:
-        print("please select a valid option")
-
-def order_operation(mode):
-    if mode.lower() == "dine":
-        date_of_booking = input("Please enter the Date of booking for dine in:12/11/2022\n")
-        time_of_booking = input("Please enter the Time of Booking for the Dine in 16:00\n")
-        persons = input("Please enter the number of persons:5\n")
-    elif (mode.lower() == "pick up"):
-        date_of_pickup = input("Please enter the Date of Pick up:12/11/2022\n")
-        time_of_pickup = input("Please enter the time of Pick up 17:00\n")
-        persons = input("Please enter the number of Persons:XXXXX\n")
-    elif (mode.lower() == "delivery"):
-        date_of_pickup = input("Please enter the Date of Delivery:12/11/2022\n")
-        time_of_pickup = input("Please enter the time of Delivery 18:00\n")
-        distance = input("Please enter the Distance from the restaurant:8\n")
-        if int(distance) > 10:
-            print("More than the applicable limits, Please you have to pickup order")
-    else:
-        print("please enter a valid option")
-    end_msg = """Thank you for your Order, Your Order has been confirmed.
- Your order id is S001
- Main Menu..."""
-    print (end_msg)
-
-def stat():
-    stat_options = """Please Enter the Option to Print the Statitics.
- 1 - All Dine in Orders.
- 2 - All Pick up Orders.
- 3 - All Deliveries.
- 4 - All Orders (Asccending Order).
- 5 - Total Amount Spent on All Orders.
- 6 - To go to Previous Menu.
- """
-    stat_options_val = input(stat_options)
-    if (stat_options_val in ["1", "2", "3", "4", "5", "6"] ):
-        if (stat_options_val == "6"):
-            print("loading previous menu...")
-        elif (stat_options_val == "5"):
-            print("""Option 5: Total Amount Spent(All Type of Orders i.e.,
- Dine in, Deliveries and Pickups)
- Total amount spent on all orders AUD: 56.0""")
-        elif (stat_options_val in ["1", "2", "3", "4"]):
-            print(f"""Option 1: All {stat_options_val} in orders Output:
-    Order ID    Date    Total Amount Paid   Type of Order""")
-    else:
-        print("you entered the wrong option")
 
 
-class Restaurant:
+
+
+
+class Calculator:
+        def total_price_in_cart(self, cart):
+            amount = 0
+            for item in cart:
+                amount += item["price"]
+            return amount
+            
+
+class Restaurant(Calculator):
     users = []
     logged_in_user = ""
 
     cart = []
+    id_count = 0
+
+    all_dine_in_order = []
+    all_pick_up_order = []
+    all_delivery_order = []
+
+    order_mode = ""
 
     food_item = {
         "1": {
@@ -232,21 +196,70 @@ class Restaurant:
         if option == "2.1":
             self.ordering()
         elif option == "2.2":
-            print("Printing Statistics...")
+            self.stat()
         elif option == "2.3":
             print("Loging out...")
             self.landing_page()
         else:
             print("Please select a valid option");
 
+    def stat(self):
+        stat_options = """Please Enter the Option to Print the Statitics.
+ 1 - All Dine in Orders.
+ 2 - All Pick up Orders.
+ 3 - All Deliveries.
+ 4 - All Orders (Ascending Order).
+ 5 - Total Amount Spent on All Orders.
+ 6 - To go to Previous Menu.
+    """
+        stat_options_val = input(stat_options)
+        if (stat_options_val in ["1", "2", "3", "4", "5", "6"] ):
+            if (stat_options_val == "6"):
+                self.dashboard()
+            elif (stat_options_val == "5"):
+                all_order = self.all_delivery_order + self.all_dine_in_order + self.all_pick_up_order
+                total_value = self.total_price_in_cart(all_order)
+                print(f"""Option 5: Total Amount Spent(All Type of Orders i.e.,
+    Dine in, Deliveries and Pickups)
+    Total amount spent on all orders AUD: {total_value}""")
+            elif (stat_options_val in ["1", "2", "3", "4"]):
+                order_type = {
+                    "1": "Dine in",
+                    "2": "Pick up",
+                    "3": "delivery",
+                    "4": "desce"
+                }
+
+                if (stat_options_val == "1"):
+                    print(f"""Option 1: All {stat_options_val} in orders Output:
+ Order ID    Date    Total Amount Paid   Type of Order""")
+                    for order in self.all_dine_in_order:
+                        id = order["price"]
+                        price = order["price"]
+                        date = order["date"]
+                        print(f"""{id}        {date}     {price}   Dine in""")
+                if (stat_options_val == "2"):
+                    print(f"""Option 1: All {stat_options_val} in orders Output:
+ Order ID    Date    Total Amount Paid   Type of Order""")
+                    for order in self.all_pick_up_order:
+                        id = order["price"]
+                        price = order["price"]
+                        date = order["date"]
+                        print(f"""{id}        {date}     {price}   """)
+                        
+        else:
+            print("you entered the wrong option")
+
     def ordering(self):
         order_option = input(f"\n Please Enter 1 for Dine in.\n Please Enter 2 for Order Online.\n Please Enter 3 to go to Login page\n")
 
         if order_option == "2":
-            #move the user to order online
+            # move the user to order online
             self.order_online()
         elif (order_option == "1"):
-            pass
+            #set user ordering mode and move user for dine
+            self.order_mode = "dine in"
+            self.menu("1")
         elif (order_option == "3"):
             self.dashboard()
         else:
@@ -258,7 +271,12 @@ class Restaurant:
  Enter 2 for Home Delivery.
  Enter 3 to go to previous Menu."""
         order_option_value = input(order_option)
-        if (order_option_value == "1" or order_option_value == "2"):            
+        if (order_option_value == "1"):
+            #set order mode
+            self.order_mode =  "click and collect"           
+            self.menu(order_option_value)
+        elif (order_option_value == "2"):
+            self.order_mode =  "delivery mode"           
             self.menu(order_option_value)
         elif(order_option_value == 3):
             self.ordering()
@@ -302,25 +320,43 @@ class Restaurant:
             print(self.cart)
             self.drink_menu(order_type)
         elif drink_option_value == "4":
-            """check for order typer if pick up or deliery"""
+            """check for order typer if pick up or delivery"""
+            check_out = """Please Enter Y to proceed to Checkout or
+ Enter N to cancel the order """
+            check_out_value = input(check_out)
             if (order_type == "2"):
                 for user in self.users:
                     if(user["mobile"] == self.logged_in_user):
                         if (user["address"] == ""):
                             self.no_address_alert()
+                            self.drink_menu(order_type)
                     else:
                         print("smart anon")
                         self.landing_page()
-            else: 
-                check_out = """Please Enter Y to proceed to Checkout or
- Enter N to cancel the order """
-                check_out_value = input(check_out)
-                if check_out_value.lower() == "y":
-                    print("Proceeding to checkout")
-                elif check_out_value.lower() == "n":
-                    print("cancling check out")
+                 
+            if check_out_value.lower() == "y":
+                """get list of options the person has bought
+                and calculate values"""
+                if (self.order_mode == "dine in"):
+                    cart_bill = self.total_price_in_cart(self.cart)
+                    charges = cart_bill * (15/100)
+                    plus_charges = cart_bill + charges
+                    print(f"Your total payble amount is: {plus_charges} inclusing AUD {charges} for Service Charges")
+                    self.order_operation(self.order_mode, plus_charges)
+                elif (self.order_mode == "click and collect"):
+                    print(f"Your total payble is: {self.total_price_in_cart(self.cart)}")
+                    self.order_operation(self.order_mode, self.total_price_in_cart(self.cart))
+                elif (self.order_mode == "delivery"):
+                    bill = self.total_price_in_cart(self.cart)
+                    print(f"Your total payble is: {bill} and there will be an additional charges for delivery.")
+                    self.order_operation(self.order_mode, bill)
                 else:
                     print("please select a valid option")
+            elif check_out_value.lower() == "n":
+                print("cancling check out")
+            else:
+                print("please select a valid option")
+
 
     def no_address_alert(self):
         msg = """You have not mentioned your address, while signing up.
@@ -346,6 +382,111 @@ class Restaurant:
             self.ordering()
         else:
             print("please enter a valid option")
+    
+    def order_operation(self, mode, plus_service):
+        if mode.lower() == "dine in":
+            date_of_booking = input("Please enter the Date of booking for dine in:12/11/2022\n")
+            time_of_booking = input("Please enter the Time of Booking for the Dine in 16:00\n")
+            persons = input("Please enter the number of persons:5\n")
+            #check if user entered a numbers
+            if (persons.isnumeric() and date_of_booking.count("/") == 2 and (":" in time_of_booking)):
+                date_to_list = date_of_booking.split("/")
+                for i in date_to_list:
+                    if i.isnumeric():
+                        pass
+
+                    else:
+                        print("Please enter valid date 12/11/2022")
+                        self.order_operation(mode, plus_service)
+                id = "S{:03d}".format(self.id_count)
+                self.id_count += 1
+                new_dine_order = {
+                    "date": date_of_booking,
+                    "time": time_of_booking,
+                    "price": plus_service * int(persons)
+                }
+                self.all_dine_in_order.append(new_dine_order)
+                print("Thank you for your Order, Your Order has been confirmed.")
+                self.dashboard()
+                
+            else:
+                print("please enter a valid order")
+                self.order_operation(mode, plus_service)
+        elif (mode.lower() == "click and collect"):
+            date_of_pickup = input("Please enter the Date of Pick up:12/11/2022\n")
+            time_of_pickup = input("Please enter the time of Pick up 17:00\n")
+            persons = input("Please enter the number of Persons:XXXXX\n")
+            if (persons.isnumeric() and date_of_pickup.count("/") == 2 and (":" in time_of_pickup)):
+                date_to_list = date_of_pickup.split("/")
+                for i in date_to_list:
+                    if i.isnumeric():
+                        pass
+                        
+                    else:
+                        print("Please enter valid date 12/11/2022")
+                        self.order_operation(mode, plus_service)
+                id = "S{:03d}".format(self.id_count)
+                self.id_count += 1
+                new_pickup_order = {
+                    "id": id,
+                    "date": date_of_pickup,
+                    "time": time_of_pickup,
+                    "price":plus_service * int(persons)
+                }
+                self.all_pick_up_order.append(new_pickup_order)
+                print("Thank you for your Order, Your Order has been confirmed.")
+                self.dashboard()
+                
+            else:
+                print("please enter a valid order")
+                self.order_operation(mode, plus_service)
+        elif (mode.lower() == "delivery"):
+            date_of_pickup = input("Please enter the Date of Delivery:12/11/2022\n")
+            time_of_pickup = input("Please enter the time of Delivery 18:00\n")
+            distance = input("Please enter the Distance from the restaurant:8\n")
+            if int(distance) > 15:
+                print("More than the applicable limits, Please you have to pickup order")
+            else:
+                if (distance.isnumeric() and date_of_pickup.count("/") == 2 and (":" in time_of_pickup)):
+                    date_to_list = date_of_pickup.split("/")
+                    for i in date_to_list:
+                        if i.isnumeric():
+                            pass
+                        else:
+                            print("Please enter valid date 12/11/2022")
+                            self.order_operation(mode, plus_service)
+                    if (int(distance) < 6):
+                        plus_service += 5
+                    elif (int(distance) < 11):
+                        plus_service += 10
+                    elif (int(distance) < 15):
+                        plus_service += 18
+                    else:
+                        pass
+                    id = "S{:03d}".format(self.id_count)
+                    self.id_count += 1
+                    new_delivery_order = {
+                        "id": id,
+                        "date": date_of_pickup,
+                        "time": time_of_pickup,
+                        "persons": int(distance),
+                        "price": plus_service
+                    }
+                    self.all_delivery_order.append(new_delivery_order)
+                    print("Thank you for your Order, Your Order has been confirmed.")
+                    self.dashboard()
+                    
+                else:
+                    print("please enter a valid order")
+                    self.order_operation(mode, plus_service)
+        else:
+            print("please enter a valid option")
+        end_msg = """Thank you for your Order, Your Order has been confirmed."""
+        print (end_msg)
+        self.dashboard()
+
+    
+
 
 x = Restaurant()
 x.landing_page()
